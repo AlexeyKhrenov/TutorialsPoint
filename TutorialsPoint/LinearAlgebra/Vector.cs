096caf9a9f5ac1b5f01344661dacbab498cd4e33
Алекс <@DESKTOP-MVPJD5D>
2019-07-06 23:01:33 +0300

    Implemented Vector for LinearAlgebra

---
+++ TutorialsPoint/LinearAlgebra/Vector.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialsPoint.LinearAlgebra
+{
+    public class Vector
+    {
+        public int Dimension => array.Length;
+
+        private double[] array;
+
+        public Vector(int dimension)
+        {
+            array = new double[dimension];
+        }
+
+        public Vector(double[] array)
+        {
+            this.array = array;
+        }
+
+        public Vector FindPerpendicular(Vector b)
+        {
+            double _AB = this * b;
+            if (_AB == 0)
+            {
+                return null;
+            }
+
+            double _BB = b * b;
+            if (_BB == null)
+            {
+                return null;
+            }
+
+            double alpha = (double)_AB / (double)_BB;
+            return this - b * alpha;
+        }
+
+        public static double operator * (Vector a, Vector b)
+        {
+            if (a.Dimension != b.Dimension)
+            {
+                throw new ArgumentException();
+            }
+
+            double result = 0;
+
+            for (var i = 0; i < a.Dimension; i++)
+            {
+                result += a.array[i] * b.array[i];
+            }
+
+            return result;
+        }
+
+        public static Vector operator * (Vector a, double scalar)
+        {
+            var c = new Vector(a.Dimension);
+            c.AddVector(a);
+            c.Multiplication(scalar);
+            return c;
+        }
+
+        public static Vector operator + (Vector a, Vector b)
+        {
+            var c = new Vector(a.Dimension);
+            c.AddVector(a);
+            c.AddVector(b);
+            return c;
+        }
+
+        public static Vector operator - (Vector a, Vector b)
+        {
+            var c = new Vector(a.Dimension);
+            c.AddVector(a);
+            c.MinusVector(b);
+            return c;
+        }
+
+        private void AddVector(Vector b)
+        {
+            if (b.Dimension != Dimension)
+            {
+                throw new ArgumentException();
+            }
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] + b.array[i];
+            }
+        }
+
+        private void MinusVector(Vector b)
+        {
+            if (b.Dimension != Dimension)
+            {
+                throw new ArgumentException();
+            }
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] - b.array[i];
+            }
+        }
+
+        private void Multiplication(double scalar)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * scalar;
+            }
+        }
+
+        private double Sum()
+        {
+            double result = 0;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+
+            return result;
+        }
+    }
+}
--- TutorialsPoint/Program.cs
+++ TutorialsPoint/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TutorialsPoint.Groups.Permutations;
+using TutorialsPoint.LinearAlgebra;
 
 namespace TutorialsPoint
 {
@@ -12,9 +13,28 @@ namespace TutorialsPoint
     {
         public static void Main(string[] args)
         {
-            var s4 = new SymmetricGroup(4);
+            var f1 = new Vector(new double[] { 1, 2, 7 });
+            var e1 = new Vector(new double[] { 1, 1, -2 });
+            var e2 = new Vector(new double[] { 1, -1, 4 });
 
-            var i = "";
+            e2 = e2.FindPerpendicular(e1);
+
+            var perpendicular1 = f1.FindPerpendicular(e1);
+
+            var _f1 = f1 - perpendicular1;
+
+            var perpendicular2 = f1.FindPerpendicular(e2);
+
+            _f1 = _f1 - perpendicular2;
+
+            var result = f1 - _f1;
+
+            var check1 = _f1 * e1;
+            var check2 = _f1 * e2;
+
+            var resultLength = result * result;
+
+            resultLength = 0;
         }
     }
 }
