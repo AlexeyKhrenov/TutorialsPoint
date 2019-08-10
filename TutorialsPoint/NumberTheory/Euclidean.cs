8a95f65812bb857e5003bfabf2aeeed808c9deaf
alexey.khrenov <alexey.khrenov@firstlinesoftware.com>
2019-08-10 22:24:38 +1000

    Implemented straight Euclidean algorithm

---
+++ TutorialsPoint/NumberTheory/Euclidean.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialsPoint.NumberTheory
+{
+    public class Euclidean
+    {
+        public static int StraightEuclideanAlgorithm(int a, int b)
+        {
+            if(a <= 0 || b <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            while (true)
+            {
+                var result = CheckForDivision(a, b);
+                if (result != 0){
+                    return result;
+                }
+                else
+                {
+                    if(a > b)
+                    {
+                        a = a - b;
+                    }
+                    else
+                    {
+                        b = b - a;
+                    }
+                }
+            }
+        }
+
+        private static int CheckForDivision(int a, int b)
+        {
+            if (a % b == 0)
+            {
+                return b;
+            }
+
+            if (b % a == 0)
+            {
+                return a;
+            }
+
+            return 0;
+        }
+    }
+}
--- TutorialsPoint/Program.cs
+++ TutorialsPoint/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using TutorialsPoint.Groups.Permutations;
 using TutorialsPoint.LinearAlgebra;
+using TutorialsPoint.NumberTheory;
 
 namespace TutorialsPoint
 {
@@ -13,28 +14,11 @@ namespace TutorialsPoint
     {
         public static void Main(string[] args)
         {
-            var f1 = new Vector(new double[] { 1, 2, 7 });
-            var e1 = new Vector(new double[] { 1, 1, -2 });
-            var e2 = new Vector(new double[] { 1, -1, 4 });
-
-            e2 = e2.FindPerpendicular(e1);
-
-            var perpendicular1 = f1.FindPerpendicular(e1);
-
-            var _f1 = f1 - perpendicular1;
-
-            var perpendicular2 = f1.FindPerpendicular(e2);
-
-            _f1 = _f1 - perpendicular2;
-
-            var result = f1 - _f1;
-
-            var check1 = _f1 * e1;
-            var check2 = _f1 * e2;
-
-            var resultLength = result * result;
-
-            resultLength = 0;
+            var a = 252;
+            var b = 105;
+            var result1 = Euclidean.StraightEuclideanAlgorithm(252, 105);
+            var result2 = Euclidean.StraightEuclideanAlgorithm(252, 252);
+            result1 = 0;
         }
     }
 }
